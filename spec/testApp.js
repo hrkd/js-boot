f8a42da4539556cdafbfbf872e5d874b4b5c0c17
hrkd <hiroki@hrkd.net>
2016-06-01 03:16:44 +0900

    不要なテスト削除

--- spec/testApp.js
+++ spec/testApp.js
@@ -1,9 +1,13 @@
 var assert = require('power-assert');
-var App = require('../js-source/app');
+var App = require('../source/javascript/app');
 
 describe('テスト実行', function() {
   it('app nameはthis app name', function () {
     assert(App.NAME === 'this app name');
   });
-});
 
+  it('app nameはthis app nameではない', function () {
+    assert(App.NAME !== 'this app name');
+  });
+
+});
